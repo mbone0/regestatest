d37e3a9c66d67156823726a77c4eca100940dea0
M. B <12018373+mbone0@users.noreply.github.com>
2023-04-19 16:14:02 +0200

    fix tests

--- java/src/unittests/SupplierTest.java
+++ java/src/unittests/SupplierTest.java
@@ -33,13 +33,14 @@ class SupplierTest
 	{
 		tested.addSale(new Sale(5,SaleType.QUANTITY, 50));
 		
-		OrderValidation act= tested.verifyandApplySale(new OrderValidation(0, 49, null, 100, 5, 1));
+		OrderValidation act= tested.verifyandApplySale(new OrderValidation(0, 49, null, 100*49, 5, 1));
 
 		assertEquals(4900, act.getPrice());
 		
-		act= tested.verifyandApplySale(new OrderValidation(0, 50, null, 100, 5, 1));
+		act= tested.verifyandApplySale(new OrderValidation(0, 50, null, 100*50, 5, 1));
 		
 		assertEquals(4750, act.getPrice());
+
 		
 	}
 
